e3b3351bc00e23dc323f13dfe1920915b50cc6f7
laura <laura.main99@gmail.com>
2022-02-19 22:07:18 +0000

    Created Country class

---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,26 @@
+package com.napier.sem;
+
+import java.math.BigDecimal;
+
+/**
+ * The Country entity represents a single row in the country database table
+ */
+public class Country {
+
+    public String Code;
+    public String Name;
+    public String Continent;
+    public String Region;
+    public BigDecimal SurfaceArea;
+    public short IndepYear;
+    public int Population;
+    public BigDecimal LifeExpectancy;
+    public BigDecimal GNP;
+    public BigDecimal GNPOld;
+    public String LocalName;
+    public String GovernmentForm;
+    public String HeadOfState;
+    public int Capital;
+    public String Code2;
+
+}
