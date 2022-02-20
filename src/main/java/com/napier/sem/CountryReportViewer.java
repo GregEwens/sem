01c23aa8e754756c425425882a22eadf235a56ab
laura <laura.main99@gmail.com>
2022-02-20 12:18:36 +0000

    Created the country report view.

---
+++ src/main/java/com/napier/sem/CountryReportViewer.java
@@ -0,0 +1,61 @@
+package com.napier.sem;
+
+import java.util.ArrayList;
+
+public class CountryReportViewer {
+
+    /**
+     * An instance of the Country repository class
+     */
+    private final CountryRepository countryRepository;
+
+    /**
+     * The public constructor
+     * @param countryRepo Pass an instance of the Country repository class
+     */
+    public CountryReportViewer(CountryRepository countryRepo){
+
+        this.countryRepository = countryRepo;
+    }
+
+
+    /**
+     * Displays all country ordered by population
+     */
+    public void ShowCountriesByPopulation(){
+        var countries = countryRepository.getAllCountries();
+
+        System.out.println("Report showing all countries ordered by population ascending");
+
+        displayCountries(countries);
+    }
+
+    /**
+     * Prints the details of a collection of countries
+     * @param countries The collection of countries to display
+     */
+    private void displayCountries(ArrayList<Country> countries){
+        for (var country: countries) {
+            displayCountry(country);
+        }
+    }
+
+    /**
+     * Prints the details of a single country
+     * @param ctry The country to display
+     */
+    private void displayCountry(Country ctry)
+    {
+        if (ctry != null)
+        {
+            System.out.println(
+                    ctry.Code + " "
+                            + ctry.Name + " "
+                            + ctry.Continent + "\n"
+                            + ctry.Region + "\n"
+                            + "Population:" + ctry.Population + "\n" +
+                            ctry.Capital);
+        }
+    }
+
+}
