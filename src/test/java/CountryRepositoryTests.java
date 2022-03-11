0b5df6320bc2f37f546a64592338d88af506e2ba
greg <greg.ewens@gmail.com>
2022-03-11 10:09:30 +0000

    Add second unit test class

---
+++ src/test/java/CountryRepositoryTests.java
@@ -0,0 +1,19 @@
+/**
+ * Project Name: seMethods
+ * User: Greg Ewens
+ * Date Created: 11/03/2022 09:07
+ * File Purpose: Unit Tests for CountryRepository
+ */
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CountryRepositoryTests {
+
+
+    @Test
+    void TestUnitTest(){
+        assertEquals(5, 5);
+    }
+
+}
