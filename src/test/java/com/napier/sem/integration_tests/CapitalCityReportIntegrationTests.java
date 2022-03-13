610884b16a06b19fbe5fc100bbba9178d42ade98
laura <laura.main99@gmail.com>
2022-03-13 19:31:31 +0000

    add class for capital city integration tests

---
+++ src/test/java/com/napier/sem/integration_tests/CapitalCityReportIntegrationTests.java
@@ -0,0 +1,47 @@
+package com.napier.sem.integration_tests;
+/**
+ * Project Name: seMethods
+ * Package: com.napier.sem.integration_tests
+ * User: Laura Main
+ * Date Created: 13/03/2022 19:30
+ * File Purpose: Integration Tests for capital city reports
+ */
+
+import com.napier.sem.App;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Integration Tests for capital city reports
+ */
+public class CapitalCityReportIntegrationTests {
+
+    /**
+     * The application to test
+     */
+    static App app;
+    
+    /**
+     * Set up the database connection by calling initialise method on App
+     */
+    @BeforeAll
+    static void init()
+    {
+        String[] args = new String[2];
+        args[0] = "localhost:33060";
+        args[1] = "300";
+
+        app = new App();
+        app.initialise(app, args);
+    }
+
+    /**
+     * Close the database connection after all the tests have run
+     */
+    @AfterAll
+    static void dispose(){
+        app.disconnect();
+    }
+}
